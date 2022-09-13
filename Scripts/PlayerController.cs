a4b4c7a0e3c6b293b2f0de8c2f78eae2fb241457
Aayush Shrestha <70073615+Hawtori@users.noreply.github.com>
2022-09-12 21:00:37 -0400

    velocity based movement
    doesn't work, reverting

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,53 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
- 
-    /***********************************************************************************
-                                THINGS TO DO
-    - fix jumping to feel better
-    - fix countermovement and friction stuff so movement feels better, 
-          slows and stops faster but not choppy
-    - improve overall game feel
-    
-                                MABIES
-    - add a slow effect for when landing (unless bunny hopping so falling makes player slow on impact)
-    - add crouching
-
-    ***********************************************************************************/
-
-    //assign from unity
-    public Transform playerCam;
-    public Transform orientation;
+    // control velocity directly this time //
+    public Transform eyes;
 
     public Rigidbody rb;
 
-    //look around
-    public float sensX = 50f, sensY = 50f; //so we can edit horizontal and vertical sens independently
+    //look
+    public float xSens = 20f, ySens = 20f; //let player adjust
 
     private float xRotation, desiredY;
-    private float sensMultiplier = 1f;
+    private float sensMultiplier = 1f; //our sens
 
-    //move around
-    public float moveSpeed = 2; //put high number so acceleration isn't slow
-    public float maxSpeed = 20;
-    public bool grounded;
-    //public LayerMask _isGround; //this can be used later on depending on what direction the game goes
-    private float speedMultiplier = 1; //1 for main weapon, 1.5 for pistol, 2.125 for knife or something like that
+    //move
+    public float moveSpeed = 1f;
+    public float speedMultiplier = 1f; // so we can adjust speed for scenarios (pistol, knife, gun, etc)
 
     //jump
+    public float jumpMultiplier = 2f;
+
     [SerializeField]
-    private float jumpForce = 1000f;
-    private static float jumpCD = 0.25f; //set time
-    private float jumpCdTimer = 0.25f; //timer for when to reset
+    private float jumpForce = 800f;
+    private float jumpCd = 0.25f, jumpTimer;
     private bool canJump = true;
+    private bool isGrounded;
 
     //inputs
-    private float x, y;
+    private Vector2 movementInput;
     private bool jump;
-
 
     private void Awake()
     {
@@ -56,139 +40,115 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-        //hide and lock cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     private void Update()
     {
-        //transform.localRotation = orientation.localRotation;
-        if(canJump == false)
-        {
-            if(jumpCdTimer < 0f)
+        if (!canJump)
+        { 
+            if (isGrounded && jumpTimer < 0f)
             {
                 canJump = true;
-                jumpCdTimer = jumpCD;
+                jumpTimer = jumpCd;
             }
             else
-            {
-                jumpCdTimer -= Time.deltaTime;
-            }
+                jumpTimer -= Time.deltaTime;  
         }
 
+        //Inputs
         GetInputs();
         Look();
     }
+
 
     private void FixedUpdate()
     {
         Move();
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground")) grounded = true;
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground")) grounded = false;
-    }
-
     /// <summary>
-    /// Get user inputs for movement
+    /// Takes in user input for movement
     /// </summary>
     private void GetInputs()
     {
-        x = Input.GetAxis("Horizontal"); //not axisRaw so we can potentially use controller too
-        y = Input.GetAxis("Vertical");
+        movementInput.x = Input.GetAxis("Horizontal");
+        movementInput.y = Input.GetAxis("Vertical");
         jump = Input.GetButton("Jump");
 
-        //normalize movement so diagnal movement isn't faster
-        Vector2 normalizedMovement = new Vector2(x, y);
-        normalizedMovement.Normalize();
-
-        x = normalizedMovement.x; y = normalizedMovement.y;
+        movementInput.Normalize();
     }
 
     /// <summary>
-    /// Get mouse inputs to look around
+    /// Takes in user mouse input to look around, shoot and moves camera
     /// </summary>
     private void Look()
     {
-        float mouseX = Input.GetAxis("Mouse X") * sensX * Time.fixedDeltaTime * sensMultiplier;
-        float mouseY = Input.GetAxis("Mouse Y") * sensY * Time.fixedDeltaTime * sensMultiplier;
+        float mouseX = Input.GetAxis("Mouse X") * xSens * Time.fixedDeltaTime * sensMultiplier;
+        float mouseY = Input.GetAxis("Mouse Y") * ySens * Time.fixedDeltaTime * sensMultiplier;
 
-        Vector3 rotation = playerCam.transform.localRotation.eulerAngles;
+        Vector3 rotation = eyes.transform.localRotation.eulerAngles;
         desiredY = rotation.y + mouseX;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f); //lock rotation
 
-        playerCam.transform.localRotation = Quaternion.Euler(xRotation, desiredY, 0);
-        //orientation.transform.localRotation = Quaternion.Euler(0, desiredY, 0);
+        eyes.transform.localRotation = Quaternion.Euler(xRotation, desiredY, 0);
         transform.localRotation = Quaternion.Euler(0, desiredY, 0);
     }
 
     /// <summary>
-    /// Execute the movement
+    /// Moves player based on input taken
     /// </summary>
     private void Move()
     {
-
-        Vector2 mag = FindVelRelativeToLook();
-        float xMag = mag.x, yMag = mag.y;
+        rb.velocity = Vector3.zero;
 
         if (canJump && jump) Jump();
 
-        //slow down if key released
-        //if (grounded && x == 0 && y == 0 && Mathf.Abs(rb.velocity.magnitude) > 0) { rb.velocity = rb.velocity / divFactor; divFactor = Mathf.Clamp(divFactor - 0.5f, 1f, 5f); }
-        if (Mathf.Abs(xMag) > 0.2 && Mathf.Abs(x) < 0.05f || (xMag < -0.2 && x > 0) || (xMag > 0.2 && x < 0)) rb.AddForce(moveSpeed * orientation.transform.right * -xMag * 0.2f);
-        if (Mathf.Abs(yMag) > 0.2 && Mathf.Abs(y) < 0.05f || (yMag < -0.2 && y > 0) || (yMag > 0.2 && y < 0)) rb.AddForce(moveSpeed * orientation.transform.forward * -yMag * 0.2f);
+        Vector3 movement = new Vector3(movementInput.x, 0, movementInput.y);
 
-        //don't take input if over max speed;
-        if (x > 0 && xMag > maxSpeed) x = 0;
-        if (x < 0 && xMag < -maxSpeed) x = 0;
-        if (y > 0 && yMag > maxSpeed) y = 0;
-        if (y < 0 && yMag < -maxSpeed) y = 0;
+        if (Mathf.Abs(rb.velocity.x) > 0.2 && Mathf.Abs(movementInput.x) < 0.05f || (rb.velocity.x < -0.2 && movementInput.x > 0) || (rb.velocity.x > 0.2 && movementInput.x < 0)) rb.AddForce(moveSpeed * transform.right * -rb.velocity.x * 0.2f);
+        if (Mathf.Abs(rb.velocity.y) > 0.2 && Mathf.Abs(movementInput.y) < 0.05f || (rb.velocity.y < -0.2 && movementInput.y > 0) || (rb.velocity.y > 0.2 && movementInput.y < 0)) rb.AddForce(moveSpeed * transform.forward * -rb.velocity.y * 0.2f);
 
-        rb.AddForce(orientation.transform.forward * y * moveSpeed /* Time.deltaTime */ * speedMultiplier);
-        rb.AddForce(orientation.transform.right * x * moveSpeed /** Time.deltaTime*/ * speedMultiplier);
-    }
 
-    private void Jump()
-    {
-        if(grounded && canJump)
+        if(rb.velocity.sqrMagnitude > moveSpeed * speedMultiplier)
         {
-            canJump = false;
-
-            rb.AddForce(Vector2.up * jumpForce * 2f);
-
-            Vector3 vel = rb.velocity;
-            if (rb.velocity.y < 0.5f)
-                rb.velocity = new Vector3(vel.x, 0, vel.z);
-            else if (rb.velocity.y > 0)
-                rb.velocity = new Vector3(vel.x, vel.y / 2, vel.z);
+            rb.velocity = rb.velocity.normalized * moveSpeed * speedMultiplier;
         }
+        Vector3 vel = new Vector3( movementInput.x * moveSpeed * speedMultiplier, rb.velocity.y, movementInput.y * moveSpeed * speedMultiplier);
+
+        rb.velocity = vel;
     }
 
     /// <summary>
-    /// Find velocity relative to where we're looking.
-    /// Good for vector calculations and stuff
+    /// Makes player jump if they are allowed to
     /// </summary>
-    private Vector2 FindVelRelativeToLook()
+    private void Jump()
     {
-        float lookAngle = orientation.transform.eulerAngles.y;
-        float moveAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
+        if (!canJump) return;
 
-        float u = Mathf.DeltaAngle(lookAngle, moveAngle);
-        float v = 90 - u;
+        canJump = false;
+        rb.AddForce(Vector2.up * jumpForce * jumpMultiplier);
 
-        float magnitue = rb.velocity.magnitude;
-        float yMag = magnitue * Mathf.Cos(u * Mathf.Deg2Rad);
-        float xMag = magnitue * Mathf.Cos(v * Mathf.Deg2Rad);
-
-        return new Vector2(xMag, yMag);
+        //preserve velocity 
+        Vector3 vel = rb.velocity;
+        if (rb.velocity.y < 0.5f)
+            rb.velocity = new Vector3(vel.x, 0, vel.z);
+        else if (rb.velocity.y > 0)
+            rb.velocity = new Vector3(vel.x, vel.y / 2, vel.z);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) isGrounded = false;
+    }
+
 }
+ 
